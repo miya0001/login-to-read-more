1f822d79de364337e74d679683da42587aca6270
miya <miya@wpist.me>
2013-06-26 11:20:38 +0900

    first commit

---
+++ login-to-read-more.php
@@ -0,0 +1,54 @@
+<?php
+/*
+Plugin Name: Login to read more
+Plugin URI: http://wpist.me/
+Description: Password protect on the content enclosed by the shortcode.
+Author: Takayuki Miyauchi
+Version: 0.1.0
+Author URI: http://wpist.me/
+*/
+
+new Plugin_Login_Read_More();
+
+class Plugin_Login_Read_More {
+
+function __construct()
+{
+    add_action('plugins_loaded', array($this, 'plugins_loaded'));
+}
+
+public function plugins_loaded()
+{
+    add_shortcode('auth', array($this, 'login_read_more'));
+}
+
+public function login_read_more($p, $content = null) {
+    if (is_user_logged_in() && !is_null( $content )) {
+        return $content;
+    } else {
+        extract(shortcode_atts(array(
+            'class' => apply_filters(
+                'login_to_read_more_class',
+                'btn btn-primary btn-large btn-readmore'
+            ),
+            'href' => apply_filters(
+                'login_to_read_more_href',
+                wp_login_url(get_permalink())
+            ),
+            'text' => apply_filters(
+                'login_to_read_more_text',
+                'Login to read more'
+            ),
+        ), $p));
+        return sprintf(
+            '<a class="%s" href="%s">%s</a>',
+            esc_attr($class),
+            esc_attr($href),
+            $text
+        );
+    }
+}
+
+}
+
+
